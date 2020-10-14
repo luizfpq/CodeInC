65fc085720c0b85ea9812f31beea52d26f32b73b
Luiz Quirino <luizfpq@gmail.com>
2020-10-13 22:11:30 -0300

    altera regras de negócio

--- AlgII_UFMS/Atividade Pratica I - Notação Polonesa/main.c
+++ AlgII_UFMS/Atividade Pratica I - Notação Polonesa/main.c
@@ -8,22 +8,15 @@
  * a leitura e organização do código
  */
 #include "lib/arquivos.h"
-#include "lib/identifica.h"
 #include "lib/pilhas.h"
-
-/**
- * Constantes
- */ 
-#define MAX 2147483647;
-#define MIN -2147483647;
+#include "convertePosfixa.h"
 
 
 /**
  * Registros
  */ 
 typedef struct {
-    int     tipo; // 0 para infixa, 1 para poxfixa
-    char    linha[MAX]; 
+    char    linha[100]; 
 } Notacao;
 
 int main(int argc, char const *argv[])
